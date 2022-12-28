75dfc2b81b42c675c49d453a43de28e14e4a14b4
Sonu Kumar <61611771+SonuKr95@users.noreply.github.com>
2022-12-28 12:37:53 +0530

    updated js

--- script.js
+++ script.js
@@ -3,26 +3,39 @@ const expenseName = document.getElementById("name");
 const expenseDate = document.querySelector("#date");
 const expenseAmount = document.getElementById("amount");
 const addData = document.getElementById("btn");
-const expenseData = document.querySelector(".expense-data");
+const expenseDatadiv = document.querySelector(".expense-data");
 
-function createTableHeader() {
-  createTableHeader = function () {};
+function createTable() {
+  createTable = function () {};
   const CreateexpenseTable = document.createElement("table");
   CreateexpenseTable.className = "my-expenses";
   CreateexpenseTable.insertAdjacentHTML(
     "afterbegin",
     "<tr class='table-header' ><th>Name</th><th>Date</th><th>Amount</th></tr>"
   );
-  expenseData.appendChild(CreateexpenseTable);
+  expenseDatadiv.appendChild(CreateexpenseTable);
+  // const tableData = document.querySelector(".table-data");
+  // if (tableData.childElementCount < 4) {
+  //   tableData.insertAdjacentHTML(
+  //     "beforeend",
+  //     "<td class='expense-name' ></td><td class='expense-date' ></td><td class='expense-Amount'></td></tr> <button>X</button>"
+  //   );
+  // } else if (tableData.childElementCount === 4) {
+  //   const newrow = CreateexpenseTable.insertRow();
+  //   newrow.insertAdjacentHTML(
+  //     "beforeend",
+  //     "<td class='expense-name' ></td><td class='expense-date' ></td><td class='expense-Amount'></td></tr> <button>X</button>"
+  //   );
+  // }
 }
 
 function createTableData() {
   const expenseTable = document.querySelector(".my-expenses");
-  // const tableData = document.querySelector(".table-data");
-
-  expenseTable.insertAdjacentHTML(
+  const row = expenseTable.insertRow();
+  row.className = "table-row";
+  row.insertAdjacentHTML(
     "beforeend",
-    "<tr class='table-data'><td id='expense-name' ></td><td id='expense-date' ></td><td id='expense-Amount'></td></tr>"
+    "<td class='expense-name' ></td><td class='expense-date' ></td><td class='expense-Amount'></td> <button class='delete-expense' >X </button>"
   );
 }
 
@@ -40,15 +53,44 @@ addData.addEventListener("click", () => {
     expenseName.value = "";
     expenseDate.value = "";
     expenseAmount.value = "";
-    createTableHeader();
+    createTable();
     createTableData();
 
-    const expenseNameTable = document.getElementById("expense-name");
-    const expenseDateTable = document.getElementById("expense-date");
-    const expenseAmountTable = document.getElementById("expense-Amount");
+    const expenseNameTable = document.querySelectorAll(".expense-name");
+    const expenseDateTable = document.querySelectorAll(".expense-date");
+    const expenseAmountTable = document.querySelectorAll(".expense-Amount");
 
-    expenseNameTable.insertAdjacentText("afterbegin", name);
-    expenseDateTable.insertAdjacentText("afterbegin", Date);
-    expenseAmountTable.insertAdjacentText("afterbegin", amount);
+    expenseNameTable.forEach((names) => {
+      if (names.textContent.length === 0) {
+        names.insertAdjacentText("beforeend", name);
+      }
+    });
+    expenseDateTable.forEach((dates) => {
+      if (dates.textContent.length === 0) {
+        dates.insertAdjacentText("beforeend", Date);
+      }
+    });
+    expenseAmountTable.forEach((amt) => {
+      if (amt.textContent.length === 0) {
+        amt.insertAdjacentText("beforeend", amount);
+      }
+    });
+
+    // expenseDateTable.forEach((date) => console.log(date.textContent.length));
+    // expenseAmountTable.forEach((amt) => console.log(amt.textContent.length));
+
+    // console.log(expenseDateTable);
+    // console.log(expenseAmountTable);
+  }
+});
+
+expenseDatadiv.addEventListener("click", (e) => {
+  if (e.target.className === "delete-expense") {
+    e.composedPath().forEach((ele) => {
+      if (ele.className != "table-row") {
+      } else {
+        ele.style.display = "none";
+      }
+    });
   }
 });
